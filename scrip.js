68a6dda9315fe8ea5a82a2eedfda2099aaa4dc73
brioso22 <109992325+brioso22@users.noreply.github.com>
2024-02-14 23:27:20 -0600

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -3,17 +3,20 @@
 const click1 = document.getElementById('click1');
 const click2 = document.getElementById('click2');
 const click3 = document.getElementById('click3');
+var contador = 0;
 
 click1.addEventListener('click', function () {
-    alert('hola usuario, ¡Haz hecho click en el ejemplo de click!');
-    console.log("ponganos buena nota :3");
+    contador++;
+    click1.innerText = " activar un contador =" + contador;
 });
 
+
 click2.addEventListener('click', function () {
-    this.style.backgroundColor= 'red';
+    this.style.width = '300px';
+    this.style.height = '100px';
 });
 click3.addEventListener('click',function(){
-this.style.display = 'none'
+    this.style.textDecoration ="line-through";
 
 })
 
@@ -24,15 +27,22 @@ const mouse2 = document.getElementById('mouseover2');
 const mouse3 = document.getElementById('mouseover3');
 
 mouse1.addEventListener('mouseover', function () {
-    window.alert("esta es una alerta al mover el mouse serca del cuadro")
+    // Hacer el elemento redondo
+    this.style.borderRadius = '50%';
 });
+
 mouse2.addEventListener('mouseover', function () {
-    this.style.backgroundColor = 'lightgreen';
+    // Hacer el fondo negro con texto blanco
+    this.style.backgroundColor = 'black';
+    this.style.color = 'white';
 });
 
 mouse3.addEventListener('mouseover', function () {
-    mouse3.style.cursor = 'pointer'
+    // Aumentar el tamaño del elemento
+    this.style.width = '150px';
+    this.style.height = '150px';
 });
+
 
 // Focus Example
 const focusInput = document.getElementById('focus1');
@@ -44,15 +54,14 @@ const focusExample2 = document.getElementById('focus2-2');
 const focusInput3 = document.getElementById('focus1-3');
 const focusExample3 = document.getElementById('focus2-3');
 
-
 focusInput.addEventListener('focus', function () {
-    focusExample.innerHTML += '<p>¡El campo de entrada está enfocado!</p>';
-});
+    focusExample.style.fontStyle = 'italic';
+})
 
-focusInput2.addEventListener('focus', function () {
-    focusExample2.style.backgroundColor = "gray";
-});
+focusInput2.addEventListener('focus', function () {   
+    focusExample2.style.backgroundColor = 'lightblue';
+})
+focusInput3.addEventListener('focus', function () {   
+    focusExample3.style.color = 'transparent';
+})
 
-focusInput3.addEventListener('focus', function () {
-    focusExample3.style.color ="red";
-});
